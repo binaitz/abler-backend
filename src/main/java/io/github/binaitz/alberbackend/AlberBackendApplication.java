7aad9282bd0cb84b99f3c9b3b912bd9ea6a0eb16
BeINone <sbeinone@gmail.com>
2019-06-08 14:15:12 +0900

    프로젝트 생성

---
+++ src/main/java/io/github/binaitz/alberbackend/AlberBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.binaitz.alberbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlberBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlberBackendApplication.class, args);
+	}
+
+}
