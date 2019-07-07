1908a5dba5b261374f777d5293f24da521cb479c
BeINone <sbeinone@gmail.com>
2019-07-07 16:41:05 +0900

    resolve #5: 로그인 API 추가

--- alber-api/src/main/java/io/github/binaitz/alberapi/TestController.java
+++ alber-api/src/main/java/io/github/binaitz/alberapi/TestController.java
@@ -2,14 +2,21 @@ package io.github.binaitz.alberapi;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @PreAuthorize("#oauth2.hasScope('read')")
+    @PreAuthorize("hasRole('ROLE_USER')")
     @GetMapping("/test")
     public String test() {
         return "test";
+    }
+
+    @PreAuthorize("#id == authentication.name")
+    @GetMapping("/{id}")
+    public String userMe(@PathVariable String id) {
+        return id;
     }
 }
---
+++ alber-api/src/main/java/io/github/binaitz/alberapi/user/web/SignInRequest.java
@@ -0,0 +1,10 @@
+package io.github.binaitz.alberapi.user.web;
+
+import lombok.Getter;
+
+@Getter
+public class SignInRequest {
+
+    private String id;
+    private String password;
+}
